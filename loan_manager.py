b134f8032293069bd10212f97c908981142666ef
aj <piergiaj@rose-hulman.edu>
2015-04-15 14:44:01 -0400

    fixed startup script, now kills processes when done

--- loan_manager.py
+++ loan_manager.py
@@ -1,5 +1,22 @@
 import subprocess
 
-subprocess.Popen(['python', 'server.py'])
-subprocess.Popen(['python', 'run.py'])
+p1 = subprocess.Popen(['python', 'server.py'])
+p2 = subprocess.Popen(['python', 'run.py'])
+
+import webbrowser
+import os
+new = 2
+url = os.path.join(os.getcwd(),'index.html')
+webbrowser.open(url,new=new)
+
+
+def goodbye():
+    p1.kill()
+    p2.kill()
+
+import atexit
+atexit.register(goodbye)
+
+while True:
+    pass
 
